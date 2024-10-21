c2561fbcdc558cd95fbbeacb23789537604ddd86
lillieayer <lillieayer@gmail.com>
2024-10-21 18:23:39 -0400

    updating reddit scraper

--- src/content_scrapers/reddit_scraper.py
+++ src/content_scrapers/reddit_scraper.py
@@ -1,23 +1,31 @@
 import praw
 from datetime import date
+from dotenv import load_dotenv, dotenv_values 
 
-
+# global to unlock PRAW API
+config = dotenv_values(".env") 
 ''' Class: wrapper for praw library meant to customize reddit scraper
-to contain keyword, subreddit, and then write to files'''
+to contain keyword, subreddit, and then write to files 
+    Purpose: scrapes reddit content in a specific subreddit'''
 class RedditScraper:
-    def __init__(self, reddit, subreddit, query):
+    def __init__(self, reddit, query, subreddit=None):
         # initialize scraper
         self.scraper = reddit
         self.keywords = query
         self.sub = subreddit
         self.posts = []
 
+    def search_all_reddit_by_keywords(self, keywords, limit=100):
+        for submission in self.scraper.front.hot(limit=limit):
+            pass
 
 # refine to prioritize posts with max amount of keywords
-    def find_posts_by_keyword(self, limit=100):
+    def search_subreddit_by_keywords(self, limit=100):
+        assert self.sub is not None, "Subreddit not specified, use other search method"
+        keyword_list = self.keywords.split("OR")
         subreddit = self.scraper.subreddit(self.sub)
         searchedPosts = subreddit.search(self.keywords,limit=limit)
-        keyword_list = self.keywords.split("OR")
+
         for post in searchedPosts:
          # Check if the post contains any of the keywords in the title or content
             title = post.title.lower()
@@ -68,33 +76,16 @@ class RedditScraper:
 if __name__ == "__main__":
     # set up reddit scraper
     reddit = praw.Reddit(
-        client_id="U9MHvmfw08K75qXHwiUt4w",
-        client_secret="zPuqIlajupd4wxR2a-DpM2hTtqkJXQ",
-        user_agent="MyRedditScraper/Lillie Ayer")
-    # establish keywords for each case
+        client_id=config['REDDIT_CLIENT_ID'],
+        client_secret=config['REDDIT_CLIENT_SECRET'],
+        user_agent=config['REDDIT_USER_AGENT'])
+    # establish keywords for each case has to use OR delimiter to comply with PRAW search query format
     case_1 = "immigrants OR illegals OR springfield OR border OR globalist OR crime OR fake ID"
     case_2 = "gender reassignment AND immigrant"
     case_3 = "transgender AND bathroom OR sexual assault"
     case_1_list = case_1.split("OR")
     case_2_list = case_2.split("OR")
     case_3_list = case_3.split("OR")
-    # create custom scraper objects for each subreddit/keyword combo
-    scrape1 = RedditScraper(reddit, "conservative", case_1)
-    scrape2 = RedditScraper(reddit, 'politics', case_1)
-    scrape3 = RedditScraper(reddit, 'tucker_carlson', case_1)
-    scrape4 = RedditScraper(reddit, 'immigration', case_1)
-    scrape5 = RedditScraper(reddit, "conservative", case_2)
-    scrape6 = RedditScraper(reddit, 'politics', case_2)
-    scrape7 = RedditScraper(reddit, 'immigration', case_2)
-    scrape8 = RedditScraper(reddit, 'counteveryvote', case_2)
-    # save all scrapers to loop through
-    channels = [scrape1, scrape2, scrape3, scrape4, scrape5, scrape6, scrape7]
-    current_date = date.today()
-    
-    for scraper in channels:
-        scraper.find_posts_by_keyword()
-        with open(f'{scraper.sub}_subreddit_output.txt', 'a') as file:
-            scraper.output_posts_found(file)
 
 
 
