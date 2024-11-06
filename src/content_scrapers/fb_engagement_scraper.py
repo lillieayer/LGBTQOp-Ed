689d579df53cd2d5a7699ddc0a8b9b9e98d14177
lillieayer <lillieayer@gmail.com>
2024-11-05 20:52:36 -0500

    added abstraction to location reactions xpaths by media type

--- src/content_scrapers/fb_engagement_scraper.py
+++ src/content_scrapers/fb_engagement_scraper.py
@@ -8,13 +8,39 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from dotenv import load_dotenv, dotenv_values 
 from fb_comment_scraper import *
+from enum import Enum
 
-''' Scraper gets the number of comments, reactions and shares from a post '''
+# to add abstraction, maps to index of items in reactions_path array
+class Media(Enum):
+    POST = 0
+    VIDEO = 1
 
-def get_reactions_from_post(driver:webdriver.Chrome)->str:
+''' Store xpath references to find reactions element in different types of fb media (posts, lives, reels) as keys
+and then hold their different engagements '''
+reactions_xpath = [ "//div[contains(text(), 'All reactions:')]/following-sibling::span[1]//span[text()]",
+                    "//span[@aria-label='See who reacted to this' and @role='toolbar']/following-sibling::*[1]//span[text()]"
+]
+  
+           
+def extract_engagements_from_post(driver:webdriver.Chrome, post_type:Enum)->str:
+    # for 
     try:
-        num_reactions = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//div[contains(text(), 'All reactions:')]/following-sibling::span[1]//span[text()]")))
-        return num_reactions.text
+        
+        #reactions = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, reactions_xpath[post_type.value])))
+            # number of comments logo/container may not be expanded
+         # number of shares/comments hidden in pseudo-element (css) sandwhiched between ::before and ::after tags
+             # so doesn't exist in dom, must use javascript to find element
+             # comments is first
+        engagement_elements = driver.execute_script("""
+            let metrics = [];
+            const num_comments_shares = Array.from(document.querySelectorAll('[class^="html-span"]'));
+            num_comments_shares.forEach(metric => {
+                metrics.push(metric.textContent);
+            });
+            return metrics;
+"""
+        )
+        print(engagement_elements)
     except Exception as e:
         print("Can't find reaction element!", e)
 
@@ -26,11 +52,16 @@ def get_reactions_from_video(driver:webdriver.Chrome)->str:
         print("Can't find reaction element!", e)
 
 
+def get_shares_from_post(driver:webdriver.Chrome):
+
+    pass
+
+
 if __name__ == '__main__':
     chrome_options = Options()
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get('https://www.facebook.com/watch/live/?ref=watch')
-    print(get_reactions_from_video(driver))
+    driver.get('https://www.facebook.com/corpuschristicronica/posts/pfbid031mgN8ToWXJu73vb4oFAhJoLQR4FgC4EURAKnfxFr7FTHBvdFvZWQHreVvn9TenzNl')
+    print(extract_engagements_from_post(driver, "post"))
     # Close the browser
     input("finished")
     driver.quit()
